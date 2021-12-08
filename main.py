3f2b19c569abe164b75f9212d076acc2cb2ab282
LyotoRui <moki.main@gmail.com>
2021-12-08 16:53:39 +0200

    main

---
+++ main.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+from secrets import *
+import base64
+import sqlite3
+
+class RZTKChecker():
+    def __init__(self) -> None:
+        pass
+
+class WebSiteChecker():
+    def __init__(self) -> None:
+        pass
+
